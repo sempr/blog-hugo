dfd6e17ee66df220f6867649d1a45882db89bc04
Sempr <iamsempr@gmail.com>
2018-03-25 19:09:50 +0800

    ADD: travis ci config

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "$BASH_SOURCE[0]" )" && pwd )"
+cd "$DIR"
+git submodule init
+git submodule update --remote
+mkdir -p /hugo
+wget -qO- https://github.com/gohugoio/hugo/releases/download/v0.37.1/hugo_0.37.1_Linux-64bit.tar.gz | tar xvz -C /hugo
+/hugo/hugo
