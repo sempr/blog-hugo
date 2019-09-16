2105767d183766176b3b9a81fd81437b7e1a47b3
Sempr <iamsempr@gmail.com>
2019-09-16 11:06:45 +0800

    MOD: upgrade hugo version

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ DIR="$( cd "$( dirname "$BASH_SOURCE[0]" )" && pwd )"
 cd "$DIR"
 git submodule init
 git submodule update --remote
-wget -qO- https://github.com/gohugoio/hugo/releases/download/v0.37.1/hugo_0.37.1_Linux-64bit.tar.gz | tar xvz
+wget -qO- https://github.com/gohugoio/hugo/releases/download/v0.58.2/hugo_0.58.2_Linux-64bit.tar.gz | tar xvz
 ./hugo
